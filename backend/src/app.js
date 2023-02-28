f2b2c707390cbf71922439ac73477755a0e97a27
Denny Azevedo <denny.filho@animaeducacao.com.br>
2023-02-27 21:44:16 -0300

    Model User - Controller Store

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import routes from './router';
+
+class App {
+	constructor() {
+		this.server = express();
+		mongoose.connect('mongodb+srv://denny:Me109G6@devhouse.tjrpvrj.mongodb.net/?retryWrites=true&w=majority', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		this.middlewares();
+		this.routes();
+	}
+
+	middlewares() {
+		this.server.use(express.json());
+	}
+
+	routes() {
+		this.server.use(routes);
+	}
+
+}
+
+export default new App().server;
+
---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,22 @@
+// metodos: index, show, update, store, destroy
+/* 
+	index: listagem de sessoes
+	store: criar uma sessao
+	show: quando queremos listar uma UNICA sessao
+	update: quando queremos alterar alguma sessao
+	destroy: qunado queremos deletar uma sessao
+*/
+import User from '../models/User';
+
+class SessionController {
+	async store(req, res) {
+		const { email } = req.body;
+		let user = await User.findOne({ email });
+		if (!user) {
+			user = await User.create({ email });
+		}
+		return res.json(user);
+	}
+}
+
+export default new SessionController();
