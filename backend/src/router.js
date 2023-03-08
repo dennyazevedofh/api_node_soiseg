57edded25ace47a5f4c530336c75dfc486d30193
Denny Azevedo <denny.filho@animaeducacao.com.br>
2023-03-08 20:03:30 -0300

    House, Dashboard e Reserve Controllers

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,6 +3,8 @@ import multer from 'multer';
 import uploadConfig from './config/upload';
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
+import DashboardController from './controllers/DashboardController';
+import ReserveController from './controllers/ReserveController';
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -10,5 +12,14 @@ const upload = multer(uploadConfig);
 routes.post('/sessions', SessionController.store);
 
 routes.post('/houses', upload.single('thumbnail'), HouseController.store);
+routes.get('/houses', HouseController.index);
+routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
+routes.delete('/houses', HouseController.destroy);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/houses/:house_id/reserve', ReserveController.store);
+routes.get('/reserves', ReserveController.index);
+routes.delete('/reserve/cancel', ReserveController.destroy);
 
 export default routes;
