cdebfec784c8c4d2cbe86c7a901a270988e8ba91
Denny Azevedo <denny.filho@animaeducacao.com.br>
2023-03-08 22:01:31 -0300

    Ajustes de segurança

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,14 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import path from 'path';
 import routes from './router';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 class App {
 	constructor() {
 		this.server = express();
-		mongoose.connect('mongodb+srv://denny:Me109G6@devhouse.tjrpvrj.mongodb.net/?retryWrites=true&w=majority', {
+		mongoose.connect(process.env.DATABASE, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
