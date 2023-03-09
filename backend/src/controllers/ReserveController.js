ffe995de5c21945e79da4d6bb06189b78c9a5e55
Denny Azevedo <denny.filho@animaeducacao.com.br>
2023-03-08 21:30:17 -0300

    Validações

--- backend/src/controllers/ReserveController.js
+++ backend/src/controllers/ReserveController.js
@@ -5,7 +5,7 @@ import User from '../models/User';
 class ReserveController{
 	async index(req, res) {
 		const { user_id } = req.headers;
-		const reserves = await Reseve.find({ user: user_id }).populate('house');
+		const reserves = await Reserve.find({ user: user_id }).populate('house');
 		return res.json(reserves);
 	}
 
@@ -30,7 +30,8 @@ class ReserveController{
 			house: house_id,
 			date
 		});
-		await reserve.populate('house').populate('user').execPopulate();
+		await reserve.populate('house');
+		await reserve.populate('user');
 		return res.json(reserve);
 	}
 
